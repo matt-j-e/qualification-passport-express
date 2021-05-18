4e5c510f3e0bfab3afdd6fe805fc4c760738a051
mattedwards <the.matt.edwards@googlemail.com>
2021-05-18 22:39:48 +0100

    add CORS to award router

--- src/routes/award.js
+++ src/routes/award.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const awardControllers = require('../controllers/awards.js');
+
+router.all("*", cors());
 
 router
   .route('/')
