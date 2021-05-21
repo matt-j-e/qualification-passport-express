dfe8ec7ccb229a8aed457463521218fb311bf4e7
mattedwards <the.matt.edwards@googlemail.com>
2021-05-21 06:54:51 +0100

    add Firebase Sevice Account details & createCustomToken method to the authenticateWorker endpoint

--- src/controllers/workers.js
+++ src/controllers/workers.js
@@ -1,5 +1,12 @@
 const { Worker } = require('../models');
 const helpers = require('./helpers');
+const admin = require('firebase-admin');
+const serviceAccount = require("../../service-account-file.json");
+// better solution would be to hold this path in .env
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 exports.create = (req, res) => helpers.createItem('worker', res, req.body);
 
@@ -22,12 +29,20 @@ exports.authenticateWorker = (req, res) => {
       res.status(404)
       .send({ message: "User not found" });
     } else if (worker.password === req.body.password) {
-        res.status(200)
-        .send({ message: "Authenticated" });
+        const uid = worker.id.toString();
+        admin
+          .auth()
+          .createCustomToken(uid)
+          .then((customToken) => {
+            res.status(200)
+            .json(customToken);
+          })
+          .catch((error) => {
+            console.log('Error creating custom token:', error);
+          });        
     } else {
       res.status(404)
       .send({ message: "Password does not match" });
     }
-    
   })
 };
